59edc8b9ce1cc542c2f86b3abbeb1bc22ebe2698
Ronal Mora <ronaljeider@gmail.com>
2021-10-01 23:26:21 -0500

    esqueleto de la tienda

---
+++ src/main/java/com/miTienditamarket/MiTienditaMarketApplication.java
@@ -0,0 +1,13 @@
+package com.miTienditamarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiTienditaMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiTienditaMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miTienditamarket/MiTienditaMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miTienditamarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiTienditaMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
